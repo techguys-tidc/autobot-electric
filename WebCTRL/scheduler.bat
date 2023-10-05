05fb813b0c4c7f57c4c47a0e3bfaeb2345e42779
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-10-05 10:19:20 +0700

    first commit

---
+++ WebCTRL/scheduler.bat
@@ -0,0 +1,6 @@
+@echo on
+SET UI_ROBOT_PATH="C:\Users\Autobot\AppData\Local\Programs\UiPath\Studio\UiRobot.exe"
+SET FILE_EXEC="C:\Users\Autobot\Desktop\Autobot\WebCTRL\Autobot.1.0.9.nupkg"
+
+%UI_ROBOT_PATH% -file %FILE_EXEC% 
+exit
---
+++ WebCTRL/upload.bat
@@ -0,0 +1,20 @@
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
+set CURRENT_DATE=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
+
+SET ACCESS_KEY_ID=TVUSTVS13PZ4T9QRFPU2 
+SET SECRET_ACCESS_KEY=1NTfgrYt2WUVJR0Qw5IJeSCus9Efn241wsFhRs4p  
+SET ENDPOINT=https://obs.ap-southeast-214.clouds.trueidc.com
+SET BUCKET=obs://demo-electric-automation-billing/
+SET UPLOAD_FOLDER=C:\Users\Autobot\Downloads\Autobot\
+SET DC=MTG/
+SET PROGRAM=WebCTRL/
+
+set NEW_PATH=%BUCKET%%DC%%PROGRAM%%CURRENT_DATE%
+
+obsutil config -i=%ACCESS_KEY_ID% -k=%SECRET_ACCESS_KEY% -e=%ENDPOINT%
+
+obsutil mkdir %NEW_PATH%
+
+obsutil cp %UPLOAD_FOLDER% %NEW_PATH% -r -f -flat
+
+del /S /q %UPLOAD_FOLDER%*
