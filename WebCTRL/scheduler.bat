5eeaee312a5fde850fcc5fa36fb2559e430ddaba
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-10-26 12:25:57 +0700

    publish 1.0.19 fix cert invalid

--- WebCTRL/scheduler.bat
+++ WebCTRL/scheduler.bat
@@ -1,6 +1,6 @@
 @echo on
 SET UI_ROBOT_PATH="C:\Users\Autobot\AppData\Local\Programs\UiPath\Studio\UiRobot.exe"
-SET FILE_EXEC="C:\Users\Autobot\Desktop\Autobot\WebCTRL\Autobot.1.0.18.nupkg"
+SET FILE_EXEC="C:\Users\Autobot\Desktop\Autobot\WebCTRL\Autobot.1.0.19.nupkg"
 
 %UI_ROBOT_PATH% -file %FILE_EXEC% 
 exit
